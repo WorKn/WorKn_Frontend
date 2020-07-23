5b296d07a32b30d0f339f99d980b0bd3008aeed9
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-07-23 16:59:38 -0400

    Added test message

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello WorKn.</p>
+        <p>Hello Test.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
