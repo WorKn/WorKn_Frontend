c77f32ace687248974844fcc1c2f8a028d4e6b9a
PdMendoza <pedromendozaagramonte@gmail.com>
2020-08-04 12:15:14 -0400

    Creacion del componente Carousel

--- src/pages/landing-page/LandingPage.js
+++ src/pages/landing-page/LandingPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./LandingPage-Style.css";
-
+import DemoCarousel from "../../components/carousel-components/Carousel.jsx";
 
 function LandingPage() {
   return (
     <div>
       <h1>This is our landing page</h1>
       <button>Tester</button>
+      <DemoCarousel />
     </div>
   );
 }
