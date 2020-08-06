7b12a14fe91b334d88011120db2099f1a9fa04e0
PdMendoza <pedromendozaagramonte@gmail.com>
2020-08-06 11:21:50 -0400

    El carousel es eliminado, y vuelve a ser un banner

--- src/pages/landing-page/LandingPage.js
+++ src/pages/landing-page/LandingPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./LandingPage-Style.css";
-import DemoCarousel from "../../components/carousel-components/Carousel.jsx";
+import Banner from "../../components/banner-components/Banner.jsx";
+
 
 function LandingPage() {
   return (
     <div>
       <h1>This is our landing page</h1>
       <button>Tester</button>
-      <DemoCarousel />
+      <Banner image={"kiwVnMm.png"} />
     </div>
   );
 }
