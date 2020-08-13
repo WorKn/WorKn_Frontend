cdb9fd585178f01889eae275c4e22dfc89341b15
1074457 <1074457@est.intec.edu.do>
2020-08-12 19:08:33 -0700

    Created the CTA Component for further improvement

---
+++ src/components/cta-components/CTAComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./CTAComponent-Style.css";
+
+const CTAComponent = ({
+  params: {imageUrl },
+}) => (
+  <div className="jumbotron-container">
+
+    {imageUrl ? (
+        <div className="jumbotron-container__image">
+            <img src={imageUrl} alt="A CTA" />
+        </div>
+    ) : null}
+    <div className="jumbotron-container__jumbotron-text">
+      <h1 className="jumbotron-container__title">En Workn buscamos ayudarte</h1>
+      <p className="jumbotron-container__description">Encargándonos de unir automáticamente <b>profesionales</b>, <b>empresas</b> y <b>freelancers</b> en nuestra plataforma 
+      mediante <b>ofertas</b> que nos permiten saber lo que necesitas y dónde te necesitan.
+      </p>
+      
+    </div>
+
+  </div>
+);
+
+export default CTAComponent;
+
