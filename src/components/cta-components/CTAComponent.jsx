3d0fa1fcf139e745c82e21cfb629afde79329f05
1074457 <1074457@est.intec.edu.do>
2020-08-14 02:18:32 -0700

    Created CTAComponent and its styles for responsive design

--- src/components/cta-components/CTAComponent.jsx
+++ src/components/cta-components/CTAComponent.jsx
@@ -3,22 +3,30 @@ import React from "react";
 import "./CTAComponent-Style.css";
 
 const CTAComponent = ({
-  params: { h1Text, pText, bText, imageUrl, inverted },
+  params: { imageUrl }
 }) => (
   <div className="jumbotron-container">
+
+    <div className="jumbotron-container__jumbotron-text">
+      <h1 className="jumbotron-container__title">De forma rápida y sencilla</h1>
+      <p className="jumbotron-container__description">Crea y encuentra ofertas de trabajo perfectas para tus necesidades.</p>
+      <a href="/loginpage">
+          <div className="jumbotron-container__ctabutton">
+            <img
+              src="https://i.imgur.com/SE2JiQf.png"
+              className="button-image"
+              alt="icon"
+            ></img>
+            Únete o inicia sesión
+          </div>
+        </a>
+    </div>
 
     {imageUrl ? (
         <div className="jumbotron-container__image">
             <img src={imageUrl} alt="A CTA" />
         </div>
     ) : null}
-    <div className="jumbotron-container__jumbotron-text">
-      <h1 className="jumbotron-container__title">En Workn buscamos ayudarte</h1>
-      <p className="jumbotron-container__description">Encargándonos de unir automáticamente <b>profesionales</b>, <b>empresas</b> y <b>freelancers</b> en nuestra plataforma 
-      mediante <b>ofertas</b> que nos permiten saber lo que necesitas y dónde te necesitan.
-      </p>
-      
-    </div>
 
   </div>
 );
