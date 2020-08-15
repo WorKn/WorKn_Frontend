a1c502dae8ca063bba7d5c8ab43097c1bd51c138
PdMendoza <pedromendozaagramonte@gmail.com>
2020-08-15 18:28:28 -0400

    fixed naming conventions with BEM, and fixed issues with sizing

--- src/components/footer-components/Footer.jsx
+++ src/components/footer-components/Footer.jsx
@@ -7,62 +7,62 @@ const Footer = () => {
   return (
     <div className="Footer">
       <div className="right-foot">
-        <div className="up-text">
+        <div className="right-foot__right-up">
           Email:
           <a href="mailto:workninfo@gmail.com">workninfo@gmail.com</a>
         </div>
-        <div className="down-text">
-          <Link className="utobj" target="_blank" to="/Terms">
+        <div className="right-foot__right-down">
+          <Link className="right-foot__utobj" target="_blank" to="/Terms">
             Terms of Service.
           </Link>
-          <Link className="utobj" target="_blank" to="/Privacy">
+          <Link className="right-foot__utobj" target="_blank" to="/Privacy">
             Privacy Policy.
           </Link>
-          <div className="Rights">
-            &copy;{new Date().getFullYear()} All rights reserved.
+          <div className="right-foot__Rights">
+            &copy;{new Date().getFullYear()} WorKn All rights reserved.
           </div>
         </div>
       </div>
       <div className="left-foot">
-        <div className="up-content">
-          <div className="flogo">
+        <div className="left-foot__up-content">
+          <div className="left-foot__logo">
             <Link to="/">
               <img src="https://imgur.com/21FKDzL.png" alt="workn-logo" />
             </Link>
           </div>
-          <div className="fnavbar">
-            <Link className="fobj" target="_blank" to="/Ofertas">
+          <div className="left-foot__navbar">
+            <Link className="left-foot__nobj" target="_blank" to="/Ofertas">
               Ofertas
             </Link>
-            <Link className="fobj" target="_blank" to="/Exploracion">
+            <Link className="left-foot__nobj" target="_blank" to="/Exploracion">
               Exploración
             </Link>
-            <Link className="fobj" target="_blank" to="/Resumen">
+            <Link className="left-foot__nobj" target="_blank" to="/Resumen">
               Resumen
             </Link>
-            <Link className="fobj" target="_blank" to="/Mensajeria">
+            <Link className="left-foot__nobj" target="_blank" to="/Mensajeria">
               Mensajería
             </Link>
           </div>
         </div>
-        <div className="middle-content">
-          <div>
+        <div className="left-foot__middle-content">
+          <div className="left-foot__icon-container">
             <Icon
               path={"https://www.facebook.com/WorKn"}
               media={"XL6Cvrz.png"}
             />
           </div>
-          <div>
+          <div className="left-foot__icon-container">
             <Icon path={"https://twitter.com/WorKn"} media={"3mO0prt.png"} />
           </div>
-          <div>
+          <div className="left-foot__icon-container">
             <Icon
               path={"https://www.instagram.com/WorKn/"}
               media={"uxM8asb.png"}
             />
           </div>
         </div>
-        <div className="down-content">
+        <div className="left-foot__down-content">
           <span>Desarrollado en la Republica Dominicana, SD</span>
         </div>
       </div>
