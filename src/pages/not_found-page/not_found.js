946abbe5885e8781373188b212cee37eb9d4d40e
PdMendoza <pedromendozaagramonte@gmail.com>
2020-08-28 20:58:21 -0400

    Replace 404 text with a 404 picture

--- src/pages/not_found-page/not_found.js
+++ src/pages/not_found-page/not_found.js
@@ -6,7 +6,11 @@ const NotFound = () => {
   return (
     <div className="not_found-page">
       <div className="not_found-container">
-        <h1>404</h1>
+        <img
+          src="https://i.imgur.com/siTaWZ0.png"
+          alt="404_image"
+          className="not_found-image"
+        />
         <h2>Esta pagina no existe</h2>
         <p>Lo sentimos, la pagina solicitada no existe</p>
         <a href="/loginpage" className="not_found-container__button">
