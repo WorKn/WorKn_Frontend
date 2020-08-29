eb095c50c52966d524f0c2a025cab7e584dde09a
Albert <albertariel2013@hotmail.com>
2020-08-29 16:01:32 -0500

    Made profile page button to show depending on user status

--- src/components/navbar-components/Navbar.jsx
+++ src/components/navbar-components/Navbar.jsx
@@ -4,43 +4,73 @@ import './Navbar-Style.css';
 
 import { Link } from "react-router-dom";
 
+import auth from '../../utils/authHelper';
 
-const Navbar = () => (
-    <div className="navbar">
-        <div className="navbar__left-items"> 
 
-            <Link to="/" className="navbar__link">
-                    <img className="navbar__logo" src="https://i.imgur.com/klMjRck.png" alt=""/>
-            </Link>
 
-            <Link className="navbar__link" to="#">
-                Ofertas
-            </Link>
-            <Link className="navbar__link" to="#">
-                Exploración
-            </Link>
-            <Link className="navbar__link" to="#">
-                Resumen
-            </Link>
-            <Link className="navbar__link" to="#">
-                Mensajeria
-            </Link>
+const Navbar = () => {
 
-        </div>
+    const isLoggedIn = auth.isAuthenticated();
 
-        <div className="navbar__right-items">
-            <Link className="navbar__link navbar__link--highlighted" to="#">
+    return (
+        <div className="navbar">
+            <div className="navbar__left-items"> 
+
+                <Link to="/" className="navbar__link">
+                        <img className="navbar__logo" src="https://i.imgur.com/klMjRck.png" alt=""/>
+                </Link>
+
+                <Link className="navbar__link" to="#">
+                    Ofertas
+                </Link>
+                <Link className="navbar__link" to="#">
+                    Exploración
+                </Link>
+                <Link className="navbar__link" to="#">
+                    Resumen
+                </Link>
+                <Link className="navbar__link" to="#">
+                    Mensajeria
+                </Link>
+
+            </div>
+            
+            {
+                isLoggedIn ? (
+                <div className="navbar__right-items">
+                    <button className="navbar__profile-button">
+                        <span>Perfil</span>
+                        <div className="navbar__img-holder">
+                            <img src="https://i0.wp.com/postmatura.al/wp-content/uploads/2018/10/blank-profile-picture-png.png?fit=512%2C512&ssl=1" alt="" className="navbar__hamburger"/>
+                        </div>
+                    </button>
+                </div>
+                
+                    
+            ) : (
+                <div className="navbar__right-items">
+                    <Link className="navbar__link navbar__link--highlighted" to="/loginpage">
+                        Iniciar sesión
+                    </Link>
+                    <Link className="navbar__link navbar__link--highlighted" to="#">
+                        Registrate
+                    </Link>
+                </div>
+            )
+            }
+                {/* <Link className="navbar__link navbar__link--highlighted" to="#">
                 Iniciar sesión
             </Link>
-            <Link className="navbar__link navbar__link--highlighted" to="#">
-                Registrate
-            </Link>
-            {/* <button>
+            
+            <button>
                 <img src="https://i.imgur.com/JCXGx2c.png" alt="" className="navbar__hamburger"/>
             </button> */}
-        </div>
+            
+        
 
     </div>
-);
+    )
+    
+};
 
 export default Navbar;
