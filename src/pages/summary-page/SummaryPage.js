a02b9d9356b88ac3f7edf063e240ef67a04f3e07
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-09-11 02:12:48 -0400

    deleted offerstrip

--- src/pages/summary-page/SummaryPage.js
+++ src/pages/summary-page/SummaryPage.js
@@ -41,7 +41,6 @@ const SummaryPage = () => {
               Estas personas están interesadas en tu oferta "Nombre Oferta #2"
             </span>
             <OfferStrip></OfferStrip>
-            <OfferStrip></OfferStrip>
             <span className="summarypage__title">
               Demostraste interés por estas personas
             </span>
