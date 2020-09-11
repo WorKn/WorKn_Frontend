14f267524d17b27bfa91e31827f4463cb905b82e
1074457 <1074457@est.intec.edu.do>
2020-09-11 01:54:05 -0700

    Successful backend connection, just need to wire up J's Components

--- src/pages/manageoffers-page/ManageOffersPage.jsx
+++ src/pages/manageoffers-page/ManageOffersPage.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./ManageOffersPage-Style.css";
 
 import Header from "../../components/navbar-components/Navbar.jsx";
 
-const ManageOffersPage = () => (
-  <div className="manageoffers-container">
-    <Header></Header>
-    <div className="manageoffers-banner">
-      {/* Hacer boton mamalon que llame a CreateOffer */}
-      <h1 className="manageoffers-banner__title">Resumen de ofertas</h1>
-    </div>
+import { getMyOffers } from "../../utils/apiRequests";
 
-    <h1>Maneje sus ofertas</h1>
-  </div>
-);
+const ManageOffersPage = () => {
+  const [myoffers, setMyOffers] = useState([]);
+  useEffect(() => {
+    getMyOffers().then((res) => {
+      console.log(res.data.data.offers);
+      setMyOffers(res.data.data.offers);
+    });
+  }, []);
+
+  return (
+    <div className="manageoffers-container">
+      <Header></Header>
+      <div className="manageoffers-banner">
+        <h1 className="manageoffers-banner__title">Resumen de ofertas</h1>
+      </div>
+      {myoffers.map((offer) => (
+        <h1 key={offer._id}>{offer.title}</h1>
+      ))}
+
+      <h1>Maneje sus ofertas</h1>
+    </div>
+  );
+};
 
 export default ManageOffersPage;
