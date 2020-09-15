3fa3621ff4d9a5851481c285067f8501c2e2c570
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-09-15 06:35:23 -0400

    did some styling changes

--- src/pages/explore-page/ExplorePage.jsx
+++ src/pages/explore-page/ExplorePage.jsx
@@ -2,39 +2,37 @@ import React from "react";
 import "./ExplorePage-Style.css";
 import Banner from "../../components/banner-components/Banner";
 import Header from "../../components/navbar-components/Navbar";
+import { useForm } from "react-hook-form";
 
 const ExplorePage = () => {
+  const { register, handleSubmit } = useForm({});
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <div className="explorepage">
         <Header />
         <Banner image={"kiwVnMm.png"} />
         <div className="explorepage__inner">
-          <div className="explorepage__wrapper">
+          <form className="explorepage__wrapper">
             <div className="explorepage__searchbox">
               <input
+                name="query"
                 type="text"
                 className="explorepage__inputsearch"
                 placeholder="Busca nombres, empresas, organizaciones o etiquetas"
+                ref={register()}
               ></input>
-              <div className="explorepage__searchbtn">
+              <div
+                onClick={handleSubmit(onSubmit)}
+                className="explorepage__searchbtn"
+              >
                 <i className="fa fa-search"></i>{" "}
               </div>
             </div>
-          </div>
+          </form>
         </div>
-        {/* <div className="explorepage__searchbar">
-            <div className="explorepage__input">
-              <i class="fa fa-search"></i>
-              <input type="text" />
-            </div>
-            <div className="explorepage__button">
-              <span>Buscar</span>
-            </div>
-          </div> */}
-        {/* <span className="explorepage__title">
-            Estas empresas están interesadas en tí
-          </span> */}
       </div>
     </div>
   );
