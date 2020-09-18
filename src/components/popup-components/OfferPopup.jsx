e307651642d4fe6f2a464b92d6ec7f87f7748f17
PdMendoza <pedromendozaagramonte@gmail.com>
2020-09-18 16:34:03 -0400

    Made some css changes and also fixed issues

--- src/components/popup-components/OfferPopup.jsx
+++ src/components/popup-components/OfferPopup.jsx
@@ -1,7 +1,4 @@
-import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import Cookies from "js-cookie";
-import Auth from "../../utils/authHelper";
+import React from "react";
 import "./OfferPopup-Style.css";
 import Tag from "../tag-components/Tag";
 
@@ -9,18 +6,27 @@ let MyDictionary = {};
 MyDictionary["free"] = "Freelancer";
 MyDictionary["fixed"] = "Fijo/Indefinido";
 
-const OfferPopup = ({ offerInfo, organizationInformation }) => {
+const OfferPopup = ({ offerInfo, organizationInformation, hide }) => {
+  const HandleHide = () => {
+    hide();
+  };
+
   return (
-    <div>
-      <div className="modal-wrapper">
-        <div className="op-wrapper__up-content">
-          {organizationInformation?.profilePicture ? (
-            <img src={organizationInformation.profilePicture} alt="Offerpp" />
-          ) : (
-            <img src="https://i.imgur.com/lcHQ2QP.jpg" alt="Offerpp" />
-          )}
-          <div className="op-wrapper__text">
-            <span>{offerInfo ? offerInfo.title : "Titulo no disponible"}</span>
+    <div className="op-wrapper">
+      <span className="op-wrapper__close" onClick={HandleHide}>
+        x
+      </span>
+      <div className="op-wrapper__up-content">
+        {organizationInformation?.profilePicture ? (
+          <img src={organizationInformation.profilePicture} alt="Offerpp" />
+        ) : (
+          <img src="https://i.imgur.com/lcHQ2QP.jpg" alt="Offerpp" />
+        )}
+        <div className="op-wrapper__text">
+          <span className="op-wrapper__title">
+            {offerInfo ? offerInfo.title : "Titulo no disponible"}
+          </span>
+          <div className="op-wrapper__bullets">
             <ul>
               <li>
                 Por <b>{organizationInformation.name}</b> en
@@ -39,44 +45,28 @@ const OfferPopup = ({ offerInfo, organizationInformation }) => {
                 {offerInfo ? offerInfo.closingDate : "Info no disponible"}
               </li>
             </ul>
-            <ul className="op-wrapper_tags">
-              {offerInfo.tags.map((tag) => (
-                <Tag
-                  key={tag._id}
-                  text={tag.name}
-                  theme="tag tag__text tag__text--gray"
-                ></Tag>
-              ))}
-            </ul>
           </div>
+          <ul className="op-wrapper_tags">
+            {offerInfo.tags.map((tag) => (
+              <Tag
+                key={tag._id}
+                text={tag.name}
+                theme="tag__small tag__text--small tag__text--gray"
+              ></Tag>
+            ))}
+          </ul>
         </div>
-        <div className="op-wrapper__down-content">
-          <span>Detalles de la oferta</span>
-          <p>
-            {offerInfo ? offerInfo.description : "Descripcion no disponible"}
-          </p>
-          <p className="op-wrapper__contact">
-            Contacto:
-            <br />
-            <a href={`mailto:${organizationInformation.email}`}>
-              {organizationInformation.email}
-            </a>
-            <b>{organizationInformation.phone}</b>
-          </p>
-          {/* <div className="op-wrapper__close-button-container">
-              <button
-                id="op-wrapper__close-button"
-                onClick={() => setState(false)}
-              >
-                {" "}
-                Cerrar
-              </button>
-              <label
-                htmlFor="op-wrapper__close-button"
-                className="op-wrapper__close-button"
-              ></label>
-            </div> */}
-        </div>
+      </div>
+      <div className="op-wrapper__down-content">
+        <span className="op-wrapper__title">Detalles de la oferta</span>
+        <p>{offerInfo ? offerInfo.description : "Descripcion no disponible"}</p>
+        <p className="op-wrapper__contact">
+          Contacto:
+          <a href={`mailto:${organizationInformation.email}`}>
+            {organizationInformation.email}
+          </a>
+          <b>{organizationInformation.phone}</b>
+        </p>
       </div>
     </div>
   );
