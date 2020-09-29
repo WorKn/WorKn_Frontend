8dd327487b629e60208821826839ca85a4eb8e96
1074457 <1074457@est.intec.edu.do>
2020-09-28 21:25:15 -0700

    If current user's email is not validated, then he is not allowed in /manageoffers. Then, he is prompted an msg to redirect to login.

--- src/pages/manageoffers-page/ManageOffersPage.jsx
+++ src/pages/manageoffers-page/ManageOffersPage.jsx
@@ -9,10 +9,12 @@ import { useModal } from "../../hooks/useModal";
 import CustomOfferStrip from "../../components/offer-components/CustomOfferStrip";
 import updateAction from "../../updateAction";
 import { useStateMachine } from "little-state-machine";
+import { Link } from "react-router-dom";
 
 const ManageOffersPage = () => {
   const [myoffers, setMyOffers] = useState([]);
   const [organizationInfo, setMyOrganization] = useState();
+  const [success, setSuccess] = useState(false);
   const { state } = useStateMachine(updateAction);
   const {
     show: showAddOfferModal,
@@ -64,37 +66,42 @@ const ManageOffersPage = () => {
 
   useEffect(() => {
     //si fallo el get my offers y el usuario actual no es tipo ofertante entonces hay que rebotarlo. Por el otro lado, si fallo el getoffers y es ofertante dejarlo entrar
-    getMyOffers().then((res) => {
-      if (!res.data && state.userInformation.userType === "offerer") {
-        // history.push("/");
-      } else if (!res.data && state.userInformation.userType !== "offerer") {
-        history.push("/loginpage");
-      } else {
-        const offers = res.data.data.offers;
-        if (offers && Array.isArray(offers)) {
-          setMyOffers(offers);
+    if (state.userInformation.isEmailValidated) {
+      setSuccess(true);
+      getMyOffers().then((res) => {
+        if (!res.data && state.userInformation.userType === "offerer") {
+          // history.push("/");
+        } else if (!res.data && state.userInformation.userType !== "offerer") {
+          history.push("/loginpage");
+        } else {
+          const offers = res.data.data.offers;
+          if (offers && Array.isArray(offers)) {
+            setMyOffers(offers);
+          }
         }
-      }
-    });
+      });
 
-    getMyOrganization().then((res) => {
-      //si el usuario actual es un ofertante sin organizacion
-      if (
-        !res.data &&
-        state.userInformation.organizationRole === "" &&
-        state.userInformation.userType === "offerer"
-      ) {
-        const organization = {
-          profilePicture: state.userInformation.profilePicture,
-        };
-        setMyOrganization(organization);
-      } else if (!res.data && state.userInformation.userType !== "offerer") {
-        history.push("/loginpage");
-      } else {
-        const organization = res.data.data.data;
-        setMyOrganization(organization);
-      }
-    });
+      getMyOrganization().then((res) => {
+        //si el usuario actual es un ofertante sin organizacion
+        if (
+          !res.data &&
+          state.userInformation.organizationRole === "" &&
+          state.userInformation.userType === "offerer"
+        ) {
+          const organization = {
+            profilePicture: state.userInformation.profilePicture,
+          };
+          setMyOrganization(organization);
+        } else if (!res.data && state.userInformation.userType !== "offerer") {
+          history.push("/loginpage");
+        } else {
+          const organization = res.data.data.data;
+          setMyOrganization(organization);
+        }
+      });
+    } else {
+      setSuccess(false);
+    }
   }, [
     history,
     state.userInformation.organizationRole,
@@ -102,7 +109,7 @@ const ManageOffersPage = () => {
     state.userInformation.userType,
   ]);
 
-  return (
+  return success ? (
     <div className="manageoffers-container">
       <Header></Header>
       <div className="manageoffers-banner">
@@ -126,6 +133,27 @@ const ManageOffersPage = () => {
         {inactiveOffers
           ? inactiveOffers
           : "Usted no ha borrado ninguna oferta aún"}
+      </div>
+    </div>
+  ) : (
+    <div className="manageoffers-nv__container">
+      <div className="manageoffers-nv__body">
+        <img
+          src="https://i.imgur.com/cDCOxmU.png"
+          alt=""
+          className="manageoffers-nv__img"
+        />
+        <h1 className="manageoffers-nv__title">
+          Su correo no ha sido validado
+        </h1>
+        <span>
+          Lo sentimos, para acceder a este contenido requerimos que su cuenta de
+          correo esté validada; aparentemente su cuenta aun no ha sido validada,
+          por favor, diríjase a su correo para continuar con el proceso.
+        </span>
+        <Link to="/" className="manageoffers-nv__button">
+          <div>Volver a inicio</div>
+        </Link>
       </div>
     </div>
   );
