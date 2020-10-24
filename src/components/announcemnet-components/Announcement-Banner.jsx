695265e5a8fa3415cc3ee4fdcb1dc52ba2be138c
PdMendoza <pedromendozaagramonte@gmail.com>
2020-10-23 21:32:18 -0400

    I made the announcement banner displayable only when the signup process is complete

--- src/components/announcemnet-components/Announcement-Banner.jsx
+++ src/components/announcemnet-components/Announcement-Banner.jsx
@@ -1,26 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./Announcement-Style.css";
 import Announcement from "./Announcement";
+import updateAction from "../../updateAction";
+import { useStateMachine } from "little-state-machine";
 
-class AnnouncementBanner extends Component {
-  state = {
-    isOpen: true,
-  };
+const AnnouncementBanner = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { state } = useStateMachine(updateAction);
 
-  render() {
-    return (
-      <div className="BA-image-container">
-        <Announcement
-          isOpen={this.state.isOpen}
-          onClose={(e) => this.setState({ isOpen: false })}
-        >
-          Casi creamos tu perfil, ayúdanos completando las siguientes
-          informaciones, mientras más precisa sea la información proporcionada
-          mejor será tu experiencia en Workn
-        </Announcement>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    if (state.userInformation.isSignupCompleted === false) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [state.userInformation.isSignupCompleted]);
+
+  return isOpen ? (
+    <div className="BA-image-container">
+      <Announcement isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        Casi creamos tu perfil, ayúdanos completando las siguientes
+        informaciones. Mientras más precisa sea la información proporcionada
+        mejor será tu experiencia en Workn.
+      </Announcement>
+    </div>
+  ) : null;
+};
 
 export default AnnouncementBanner;
