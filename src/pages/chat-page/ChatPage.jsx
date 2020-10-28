9dec371b66aadc9ea6b6e3ca752206b3f47b4b99
Yordi Ogando <yordiogando@gmail.com>
2020-10-28 18:06:29 -0400

    Checkpoint

--- src/pages/chat-page/ChatPage.jsx
+++ src/pages/chat-page/ChatPage.jsx
@@ -15,83 +15,90 @@ const HOST = "http://127.0.0.1:3000";
 const socket = socketIOClient(HOST);
 
 const ChatPage = () => {
-    const { state } = useStateMachine(updateAction);
-    const [messages, setMessages] = useState([]);
-    // const [username, setUsername] = useState([]);
-    const { register, handleSubmit, watch, reset } = useForm({});
+  const { state } = useStateMachine(updateAction);
+  const [messages, setMessages] = useState([]);
+  // const [username, setUsername] = useState([]);
+  const { register, handleSubmit, watch, reset } = useForm({});
 
-    // prompt("Hola nene");
+  // prompt("Hola nene");
 
-    const submit = (data) => {
-        createChat(message.current, state.userInformation.interactionId).then(
-            (res) => {
-                if (res.data !== undefined) {
-                    console.log(res);
-                }
-            }
-        );
-        socket.emit("chat_message", message.current);
-        console.log("Message: ", message);
-        console.log("Messagesssss:", messages);
-        // getTesting().then((res) => {
-        //   console.log(res);
-        // });
-        // message.current = reset();
-    };
-
-    // useEffect(() => {
-    //   const username = prompt("Ingrese su nombre de usuario: ");
-    //   socket.emit("username", username);
-    //   // setUsername(username);
-    // }, []);
-
-    useEffect(() => {
-        socket.on("chat_message", (data) => {
-            setMessages([...messages, data]);
-        });
-    }, [submit]);
-
-    useEffect(() => {
-        socket.on("is_online", (data) => {
-            setMessages([...messages, data]);
-        });
-    }, [submit]);
-
-    //   const addItem = () => {
-    //     setMessages([...messages, "Hola Jay"]);
-    //   };
-
-    const message = useRef({});
-    message.current = watch("message_input", "");
-
-    //   const submit = (data) => {
-    //     socket.emit("testEvent", message.current);
-    //     console.log(message);
-    //     // message.current = reset();
-    //   };
-
-    return (
-        <div className="main-div">
-            <h1>CHAT</h1>
-            {messages.map((el) => (
-                <span>{el}</span>
-            ))}
-            <form
-                action={HOST}
-                method="POST"
-                id="chatForm"
-                onSubmit={handleSubmit(submit)}
-            >
-                <input
-                    name="message_input"
-                    autocomplete="off"
-                    ref={register}
-                    autofocus="on"
-                    placeholder="type your message here..."
-                />
-                <button>Send</button>
-            </form>
-        </div>
+  const submit = (data) => {
+    createChat(message.current, state.userInformation.interactionId).then(
+      (res) => {
+        if (res.data !== undefined) {
+          console.log(res);
+        }
+      }
     );
+    // console.log(data);
+    // socket.emit("chat_message", message.current);
+    // console.log("Message: ", message);
+    // console.log("Messagesssss:", messages);
+    // getTesting().then((res) => {
+    //   console.log(res);
+    // });
+    // message.current = reset();
+  };
+
+  // useEffect(() => {
+  //   const username = prompt("Ingrese su nombre de usuario: ");
+  //   socket.emit("username", username);
+  //   // setUsername(username);
+  // }, []);
+
+  useEffect(() => {
+    socket.on("chat_message", (data) => {
+      console.log("New message: ", data);
+      setMessages([...messages, data]);
+    });
+  }, [submit]);
+
+  useEffect(() => {
+    // console.log("Joining to chat. Room Id: ", state.userInformation.email);
+    // socket.emit("join_chat", state.userInformation.email);
+  }, []);
+
+  useEffect(() => {
+    socket.on("is_online", (data) => {
+      setMessages([...messages, data]);
+    });
+  }, [submit]);
+
+  //   const addItem = () => {
+  //     setMessages([...messages, "Hola Jay"]);
+  //   };
+
+  const message = useRef({});
+  message.current = watch("message_input", "");
+
+  //   const submit = (data) => {
+  //     socket.emit("testEvent", message.current);
+  //     console.log(message);
+  //     // message.current = reset();
+  //   };
+
+  return (
+    <div className="main-div">
+      <h1>CHAT</h1>
+      {messages.map((el) => (
+        <span>{el}</span>
+      ))}
+      <form
+        action={HOST}
+        method="POST"
+        id="chatForm"
+        onSubmit={handleSubmit(submit)}
+      >
+        <input
+          name="message_input"
+          autocomplete="off"
+          ref={register}
+          autofocus="on"
+          placeholder="type your message here..."
+        />
+        <button>Send</button>
+      </form>
+    </div>
+  );
 };
 export default ChatPage;
