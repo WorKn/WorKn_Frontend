bbfa8be549d0ef87573c07122397c9dfe6b7eef8
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-10-30 19:03:19 -0400

    added tooltips to the edit popup as well

--- src/components/popup-components/EditOfferPopup.jsx
+++ src/components/popup-components/EditOfferPopup.jsx
@@ -184,12 +184,12 @@ const EditOfferPopup = ({ hide, offerInfo }) => {
               />
             </div>
             <div className="create-offer__paired-input">
-              <span>Categoría</span>
+              <span>Categoría <i className="fa fa-info-circle tooltip"><span className="tooltiptext">Las categorías te permiten filtrar los tags.</span></i></span>
               <CategoryInput></CategoryInput>
             </div>
 
             <div className="create-offer__paired-input">
-              <span>Tags</span>
+              <span>Tags <i className="fa fa-info-circle tooltip"><span className="tooltiptext">Son etiquetas que definen las habilidades que buscas para la oferta.</span></i></span>
 
               <TagsInput
                 query={`http://stagingworknbackend-env.eba-hgtcjrfm.us-east-2.elasticbeanstalk.com/api/v1/categories/${selectedCategory.value}/tags`}
