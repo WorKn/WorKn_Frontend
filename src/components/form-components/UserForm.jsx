980f8f45390655b82431e47e4897e8ff228fc15e
Jeremy Martinez <jeremy-alejandro_28@hotmail.com>
2020-11-20 10:37:59 -0400

    added edit mode

--- src/components/form-components/UserForm.jsx
+++ src/components/form-components/UserForm.jsx
@@ -41,6 +41,7 @@ const UserForm = () => {
   const [selectedCategory, setSelectedCategory] = useState([]);
   const [selectedTags, setSelectedTags] = useState([]);
   const [updated, setUpdated] = useState("");
+  const [isEditMode, setIsEditMode] = useState(false);
   const { state, action } = useStateMachine(updateAction);
   const password = useRef({});
   const { register, handleSubmit, errors, watch } = useForm({
@@ -89,6 +90,7 @@ const UserForm = () => {
         });
       }
     });
+    setIsEditMode(false);
   };
 
   useEffect(() => {
@@ -119,10 +121,143 @@ const UserForm = () => {
   return (
     <categoryContext.Provider value={{ selectedCategory, setSelectedCategory }}>
       <tagsContext.Provider value={{ selectedTags, setSelectedTags }}>
-        <form className="userform" onSubmit={handleSubmit(onSubmit)}>
           <div className="userform__LIP">
             <PicSelector isOrg={isOrg}></PicSelector>
           </div>
+          {typeof isEditMode && isEditMode === false ? (
+            <div className="userform" onSubmit={handleSubmit(onSubmit)}>
+              <div className="userform__2col">
+              <div className="userform__LIP">
+              <span className="userform__label">Nombre</span>
+              <div className="userform__placeholder">
+                  <span>{state.userInformation.name}</span>
+              </div>
+              </div>
+            <div className="userform__LIP userform__LIP--separated">
+              <span className="userform__label">Apellido</span>
+              <div className="userform__placeholder">
+                  <span>{state.userInformation.lastname}</span>
+              </div>
+            </div>
+          </div>
+          <div className="userform__LIP">
+              <span className="userform__label">Identificación</span>
+              <div className="userform__placeholder">
+                  <span>{state.userInformation.identificationNumber}</span>
+              </div>
+            </div>    
+            <div className="userform__LIP">
+              <span className="userform__label">Biografia</span>
+              <div className="userform__placeholder userform__placeholder--bio">
+                  <span>{state.userInformation.bio}</span>
+              </div>
+            </div>
+          <div className="userform__2col">
+          <div className="userform__LIP">
+              <span className="userform__label">Telefono</span>
+              <div className="userform__placeholder">
+                  <span>{state.userInformation.phone}</span>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {typeof state.userInformation !== "undefined" &&
+            state.userInformation.tags &&
+            state.userInformation.category ? (
+              <div>
+                <div className="userform__LIP">
+                  <span className="userform__label">Etiquetas</span>
+                  <div className="userform__tagscontainer">
+                    {state.userInformation.tags.map((tag) => (
+                      <Tag
+                        key={tag._id}
+                        text={tag.name}
+                        theme="tag tag__text tag__text--white"
+                      ></Tag>
+                    ))}
+                  </div>
+                </div>
+                <div className="userform__LIP">
+                  <span className="userform__label">Categoría</span>
+                  <Tag
+                    text={state.userInformation.tags[0].category.name}
+                    theme="tag tag__text tag__text--white"
+                  ></Tag>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+
+            {typeof state.userInformation.organizationRole !== "undefined" &&
+            state.userInformation.organizationRole !== "owner" &&
+            state.userInformation.userType !== "offerer" ? (
+              <div>
+                <div className="userform__footer">
+                  <span className="userform__title">
+                    Selecciona tu categoría y tus etiquetas
+                  </span>
+                  <span className="userform__text">
+                    Las etiqueta sirven para emparejarte con ofertas de trabajo
+                    y personas en tus mismas áreas de conocimiento, la categoría
+                    sirve para filtrar dichas etiquetas de una manera más
+                    precisa.
+                  </span>
+                </div>
+
+                <div className="userform__LIP">
+                  <span className="userform__label">
+                    Categoría{" "}
+                    <i className="fa fa-info-circle tooltip">
+                      <span className="tooltiptext">
+                        Las categorías te permiten filtrar los tags.
+                      </span>
+                    </i>
+                  </span>
+                  <CategoryInput></CategoryInput>
+                </div>
+                <div className="userform__LIP">
+                  <span className="userform__label">
+                    Etiquetas{" "}
+                    <i className="fa fa-info-circle tooltip">
+                      <span className="tooltiptext">
+                        Son palabras clave que definen las habilidades que
+                        tienes para ofrecer.
+                      </span>
+                    </i>
+                  </span>
+                  <TagsInput
+                    query={`http://stagingworknbackend-env.eba-hgtcjrfm.us-east-2.elasticbeanstalk.com/api/v1/categories/${selectedCategory.value}/tags`}
+                  ></TagsInput>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div className="userform__footer">
+                  <span className="userform__title">
+                    Mantén tu perfil actualizado
+                  </span>
+                  <span className="userform__text">
+                    Recuerda que esta información será vista por los usuarios
+                    que deseen interactuar contigo, mantenla actualizada y crea
+                    una descripción llamativa. Ten en cuenta que solo puedes
+                    modificar tu Identificación una vez.
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+          {/* <input
+            className="custom-button bg-green"
+            type="submit"
+            value="Viejo Guardar Perfil"
+          /> */}
+          <button className="custom-button bg-green" onClick={()=> {setIsEditMode(true)}}>Editar Perfil</button>
+
+        </div>
+          ) : (
+            <form className="userform" onSubmit={handleSubmit(onSubmit)}>
           <div className="userform__2col">
             <div className="userform__LIP">
               <span className="userform__label">Nombre</span>
@@ -331,22 +466,15 @@ const UserForm = () => {
               </div>
             )}
           </div>
-          {/* {typeof updated.data !== "undefined" &&
-            updated.data.status === "success" ? (
-              <div className="input__msg input__msg--success">
-                {updated.data.data.user.name}, tu perfil fue actualizado
-              correctamente.
-              </div>
-            ) : (
-              ""
-            )} */}
-          {/* <div className="input__msg input__msg--error">{updated.message}</div> */}
           <input
             className="custom-button bg-green"
             type="submit"
             value="Guardar Perfil"
           />
         </form>
+          )}
+          
+        
       </tagsContext.Provider>
     </categoryContext.Provider>
   );
