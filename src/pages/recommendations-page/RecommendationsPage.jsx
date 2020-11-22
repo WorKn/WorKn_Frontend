602cc66a0e0995967bba9cf656df0ede5be66685
1074457 <1074457@est.intec.edu.do>
2020-11-22 03:31:01 -0500

    Validations if user has no recommendarions (users or offers)

--- src/pages/recommendations-page/RecommendationsPage.jsx
+++ src/pages/recommendations-page/RecommendationsPage.jsx
@@ -12,6 +12,7 @@ import {
 import { useHistory } from "react-router-dom";
 import "./RecommendationsPage-Style.css";
 import RecommendationBlock from "../../components/recommendation-components/RecommendationBlock";
+import ProfileNotCompleted from "../../components/profilenotcompleted-components/ProfileNotCompleted";
 
 const RecommendationsPage = () => {
   const [userRecommendations, setUserRecommendations] = useState();
@@ -68,34 +69,82 @@ const RecommendationsPage = () => {
     <div className="recommendationspage">
       <Header />
       <Banner image={"ooyAGWN.png"} />
-      <div className="recommendationspage__container">
-        {userRecommendations && (
-          <div className="recommendationspage__body">{uRecommendations}</div>
-        )}
-        {offerRecommendations && (
-          <div className="recommendationspage__body">
-            <span className="recommendationspage__rectitle">
-              Ofertas de trabajo <span>recomendadas para ti</span>
-            </span>
-            {typeof offerRecommendations &&  (offerRecommendations?.length < 1 || offerRecommendations === undefined) ? (
-             <div className="summary__announcement">
-            <div className="summarypage__imgbg">
-              <img src="https://i.imgur.com/ESdjiH3.png" alt="applied" className="summarypage_appliedimg"></img>
+
+      {state.userInformation.isSignupCompleted ? (
+        <div className="recommendationspage__container">
+          {userRecommendations && (
+            <div className="recommendationspage__body">
+              {uRecommendations}
+              {typeof userRecommendations &&
+              (userRecommendations?.length < 1 ||
+                userRecommendations === undefined) ? (
+                <div className="summary__announcement">
+                  <div className="summarypage__imgbg">
+                    <img
+                      src="https://i.imgur.com/VhPGUOU.png"
+                      alt="applied"
+                      className="summarypage_appliedimg"
+                    ></img>
+                  </div>
+                  <div className="summary__announcementinner">
+                    <span className="summarypagea__title--dark">
+                      No hemos podido recomendar ningún usuario basado en sus
+                      ofertas.
+                    </span>
+                    <span>
+                      Asegurate de crear un perfil llamativo que atrape a los
+                      usuarios que se encuentren contigo en la plataforma.
+                      También puedes utilizar la página de Exploración para
+                      encontrar usuarios de manera manual.
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-              <div className="summary__announcementinner">
-                <span className="summarypagea__title--dark">No hemos podido generar ninguna recomendación.</span>
-                <span>Asegurate de crear un perfil llamativo que atrape a los usuarios que se encuentren contigo en la plataforma. También puedes utilizar la página de Exploración para aplicar manualmente a ofertas de trabajo.</span>
+          )}
+          {offerRecommendations && (
+            <div className="recommendationspage__body">
+              <span className="recommendationspage__rectitle">
+                Ofertas de trabajo <span>recomendadas para ti</span>
+              </span>
+              {typeof offerRecommendations &&
+              (offerRecommendations?.length < 1 ||
+                offerRecommendations === undefined) ? (
+                <div className="summary__announcement">
+                  <div className="summarypage__imgbg">
+                    <img
+                      src="https://i.imgur.com/ESdjiH3.png"
+                      alt="applied"
+                      className="summarypage_appliedimg"
+                    ></img>
+                  </div>
+                  <div className="summary__announcementinner">
+                    <span className="summarypagea__title--dark">
+                      No hemos podido generar ninguna recomendación.
+                    </span>
+                    <span>
+                      Asegurate de crear un perfil llamativo que atrape a los
+                      usuarios que se encuentren contigo en la plataforma.
+                      También puedes utilizar la página de Exploración para
+                      aplicar manualmente a ofertas de trabajo.
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="recommendationspage__offerlist">
+                {oRecommendations}
               </div>
-             </div>
-            ) : (
-              ""
-            )}
-            <div className="recommendationspage__offerlist">
-              {oRecommendations}
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      ) : (
+        <ProfileNotCompleted></ProfileNotCompleted>
+      )}
+
       <Footer></Footer>
     </div>
   );
