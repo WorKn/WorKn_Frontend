d30db4fa9e5ba44a5d589d54a39790733ed8920b
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-11-26 13:10:23 -0400

    Second code cleanse

--- src/pages/chat-page/ChatPage.jsx
+++ src/pages/chat-page/ChatPage.jsx
@@ -8,12 +8,8 @@ import Banner from "../../components/banner-components/Banner";
 import Header from "../../components/navbar-components/Navbar";
 import Footer from "../../components/footer-components/Footer";
 import { getMyChats, getChatMessages, createMessage } from "../../utils/apiRequests";
-
-
-// import { getTesting } from "../utils/apiRequests";
-
+import ScrollToBottom from "react-scroll-to-bottom";
 import "./ChatPage-Style.css";
-
 import socketIOClient from "socket.io-client";
 import interactionContext from "../../utils/interactionContext";
 import Contact from "../../components/chat-components/Contact";
@@ -27,68 +23,69 @@ const ChatPage = () => {
   const [messages, setMessages] = useState([]);
   const [chatExists, setChatExists] = useState(false);
   const [currentChat, setCurrentChat] = useState({});
-  // const [username, setUsername] = useState([]);
   const { register, handleSubmit, watch, reset } = useForm({});
-  // prompt("Hola nene");
   const submit = (data) => {
     console.log(data.message_input);
     console.log("data: " + data[1]);
     if (chatExists) {
       createMessage(data.message_input, currentChat._id).then((res) => {
-        socket.emit("join_chat", currentChat._id);
+        socket.emit("join_chat", currentChat._id); //borrar
         socket.emit("chat_message", currentChat._id, res.data.data.message);
         console.log(res);
-        data.message_input = reset();
       });
-      // socket.emit("chat_message", res.data.data.chat.id, res.data.data.lastMessage);
     } else {
       createChat(data.message_input, state.userInformation.chatPivot.interactionId).then(
         (res) => {
-          if (res.data !== undefined) {
+          if (res.data !== undefined && res.data.status === "success") {
             console.log(res);
-            if (res.data.status === "success") {
-              setCurrentChat(res.data.data.chat);
-              socket.emit("join_chat", res.data.data.chat.id);
-              socket.emit("chat_message", res.data.data.chat.id, res.data.data.lastMessage);
-              data.message_input = reset();
-            }
+            setCurrentChat(res.data.data.chat);
+            socket.emit("join_chat", res.data.data.chat.id); // borrar
+            socket.emit("chat_message", res.data.data.chat.id, res.data.data.lastMessage);
           }
         }
       );
     }
-    // console.log(data);
-    // socket.emit("chat_message", message.current);
-    // console.log("Message: ", message);
-    // console.log("Messagesssss:", messages);
-    // getTesting().then((res) => {
-    //   console.log(res);
-    // });
-    // message.current = reset();
+    data.message_input = reset();
   };
 
-  // useEffect(() => {
-  //   const username = prompt("Ingrese su nombre de usuario: ");
-  //   socket.emit("username", username);
-  //   // setUsername(username);
-  // }, []);
+  useEffect(() => {
+    getMyChats().then((res) => {
+      let myChats = res.data.data.chats;
+      const found = myChats.find(chat => chat.user.id == state.userInformation.chatPivot.userInfo.id);
+      if (found) {
+        setChatExists(true);
+        setCurrentChat(found);
+        console.log(found);
+        getChatMessages(found._id).then((res) => {
+          socket.emit("join_chat", found._id); //borrar
+          console.log(res);
+          console.log(res.data.data.chat.messages);
+          setMessages(res.data?.data.chat?.messages)
+        });
+      } else {
+        const chatPreview = {
+          user: state.userInformation.chatPivot.userInfo
+        }
+        myChats.push(chatPreview);
+      }
+      console.log(myChats);
+      setChats(myChats);
+    });
+  }, []);
 
   useEffect(() => {
     socket.on("chat_message", (data) => {
-      console.log("New message: ", data);
+      console.log("New message: ");
       setMessages([...messages, data]);
     });
-  }, [messages]);
+  }, [messages]); //Review update flag
+
 
   // useEffect(() => {
-  //   console.log("Joining to chat. Room Id: ", state.userInformation.email);
-  //   socket.emit("join_chat", state.userInformation.email);
-  // }, []);
-
-  useEffect(() => {
-    socket.on("is_online", (data) => {
-      setMessages([...messages, data]);
-    });
-  }, [submit]);
+  //   socket.on("is_online", (data) => {
+  //     setMessages([...messages, data]);
+  //   });
+  // }, [submit]);
 
   useEffect(() => {
     if (currentChat._id) {
@@ -96,61 +93,11 @@ const ChatPage = () => {
         console.log(res)
         let temporary = res.data.data.chat.messages;
         socket.emit("join_chat", currentChat._id);
-        setTimeout(() => {
-          console.log(res)
-          setMessages(temporary)
-        }, 3000);
+        setMessages(temporary)
         console.log(res)
       });
     }
   }, [currentChat]);
-
-  useEffect(() => {
-    getMyChats().then((res) => {
-      let myChats = res.data.data.chats;
-      // console.log(chats);
-      const found = myChats.find(chat => chat.user.id == state.userInformation.chatPivot.userInfo.id);
-      if (found) {
-        setChatExists(true);
-        setCurrentChat(found);
-        console.log(found);
-        getChatMessages(found._id).then((res) => {
-          socket.emit("join_chat", found._id);
-          console.log(res);
-          console.log(res.data.data.chat.messages);
-          setMessages(res.data?.data.chat?.messages)
-          // action(res.data.c)
-        });
-      } else {
-        const chatPreview = {
-          user: state.userInformation.chatPivot.userInfo
-        }
-        myChats.push(chatPreview);
-        // createChat(message.current, state.userInformation.chatPivot.interactionId).then(
-        //   (res) => {
-        //     if (res.data !== undefined) {
-
-        //     }
-        //   }
-        // );
-      }
-      console.log(myChats);
-      setChats(myChats);
-    });
-  }, []);
-
-  //   const addItem = () => {
-  //     setMessages([...messages, "Hola Jay"]);
-  //   };
-
-  // const message = useRef({});
-  // message.current = watch("message_input", "");
-
-  //   const submit = (data) => {
-  //     socket.emit("testEvent", message.current);
-  //     console.log(message);
-  //     // message.current = reset();
-  //   };
 
   return (
     <div className="chatpage">
@@ -176,12 +123,13 @@ const ChatPage = () => {
             )}
           </div>
           <div className="chat__boxright">
-            <ul className="chat__messagecontainer">
-              {messages?.map((el) => (
-                <Message message={el.message} createdAt={el.createdAt} isMyMessage={el.sender === state.userInformation._id ? true : false} ></Message>
-              ))}
-            </ul>
-
+            <ScrollToBottom mode="bottom" className="chat__messagecontainer">
+              <ul className="chat__messagecontainer">
+                {messages?.map((el) => (
+                  <Message message={el.message} createdAt={el.createdAt} isMyMessage={el.sender === state.userInformation._id ? true : false} ></Message>
+                ))}
+              </ul>
+            </ScrollToBottom>
             <div className="chat__barcontainer">
               <form
                 className="chat__form"
